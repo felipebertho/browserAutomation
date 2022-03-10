56cec5f4a290fd5a92e41f1c6bbb0f57dc932645
Fez1ca <felipebertho@icloud.com>
2022-03-10 10:17:21 -0300

    modificação readme e edição de urls para usuarios

--- scriptAutomation.py
+++ scriptAutomation.py
@@ -5,19 +5,20 @@ c = webbrowser.get('firefox')
 
 #URL para iniciar a abertura das páginas
 
-c.open_new('www.google.com.br')
-c.open_new_tab("www.linkedin.com.br")
+c.open_new('www.linkedin.com.br')
+c.open_new_tab("www.tryhackme.com")
+c.open_new_tab('www.gmail.com')
+c.open_new_tab('https://eylearning.udemy.com/organization/home/')
 
-#teste se abrirá outra aba 
 
-c.open_new('www.yahoo.com.br')
-c.open_new_tab('www.google.com.br')
 
-#teste incremento de tabs 
-c.open_new('www.youtube.com')
-c.open_new_tab('www.globo.com')
 
-#abertuira de nova janela 
+
+#Se necessário abrir outra janela com abas embutidas, somente executar os proximos comandos, usando o 'c.open_new' para abrir outra janela do navegador.
+
+#c.open_new('www.yahoo.com.br')
+#c.open_new_tab('www.google.com.br')
+
 
 
 
