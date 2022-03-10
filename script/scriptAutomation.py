78ccb189a90b732b84e82b2ac140add1e55b3f31
Fez1ca <felipebertho@icloud.com>
2022-03-10 11:21:09 -0300

    scriptBrowser.v1 BETA

--- script/scriptAutomation.py
+++ script/scriptAutomation.py
@@ -1,25 +1,21 @@
 import webbrowser
 
 
-c = webbrowser.get('firefox')
+c = webbrowser.get('firefox') #Substituir pelo nome do seu browser ou utilizar = 'windows-defauilt' para abrir com seu navegador padrão.
 
-#URL para iniciar a abertura das páginas
+#URL para iniciar a abertura das páginas = 'open_new'
+#URLs podem ser substituidas para de sua preferência.
 
-c.open_new('www.linkedin.com.br')
+c.open_new('www.linkedin.com.br') 
 c.open_new_tab("www.tryhackme.com")
 c.open_new_tab('www.gmail.com')
 c.open_new_tab('https://eylearning.udemy.com/organization/home/')
-
-
-
 
 
 #Se necessário abrir outra janela com abas embutidas, somente executar os proximos comandos, usando o 'c.open_new' para abrir outra janela do navegador.
 
 #c.open_new('www.yahoo.com.br')
 #c.open_new_tab('www.google.com.br')
-
-
 
 
 #fim 
